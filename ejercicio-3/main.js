3143cf0690d10b67afc6e2e4d4d2bf039b25bcc6
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2023-05-31 13:44:16 -0300

    TERMINADO

--- ejercicio-3/main.js
+++ ejercicio-3/main.js
@@ -1,84 +1,27 @@
-
-// let formulario= document.getElementById('formulario');
-
-
-// const operadores = {
-//   suma: 1,
-//   resta: 2,
-//   multiplicacion: 3,
-//   division: 4,
-// };
-
-// formulario.addEventListener("submit", function(e){
-//     e.preventDefault();
-//     let formulario = new FormData(e.target);
-//     let numero1=Number(formulario.get("n1"));
-//     let numero2=Number(formulario.get("n2"));
-//     let operadores=Number(formulario.get('selector'))
-//     let resultado=document.getElementById('resultado');
-
-//     switch (operadores){
-//         case operation.suma:
-//             resultado.innerHTML=Number(numero1)+Number(numero2);
-//             break;
-//         case operation.resta:
-//             resultado.innerHTML=Number(numero1)-Number(numero2);
-//             break;
-//         case operation.multiplicacion:
-//             resultado.innerHTML=Number(numero1)*Number(numero2);
-//             break;
-//         case operation.division:
-//             resultado.innerHTML=Number(numero1)/Number(numero2);
-//             break;
-//         default:resultado.innerHTML = "Error"
-
-//     }
-    
-// }
-// )
+  const boton=document.getElementById('btncalcular');
+  const $selector=document.getElementById('selector');
+  let $resultado=document.getElementById('resultado')
 
 
 
-let formulario = document.getElementById("formulario");
+boton.addEventListener('click',function(){
+  let num1 = parseFloat( document.getElementById('numero1').value);
+  let num2 = parseFloat( document.getElementById('numero2').value);
+switch ($selector.value){
+  case 'suma': $resultado.innerText= num1 + num2
+  break;
+  case 'resta':$resultado.innerText= num1-num2
+  break ;
+  case 'multiplicacion':$resultado.innerText= num1*num2
+  break ;
+  case 'division':
+    if(num2!=0 ){
+      $resultado.innerText= (num1/num2).toFixed(2)
+    }
+    else  {$resultado.innerText= "No se permite division por 0"};
+  break ;
+  default : $resultado.innerText="error"
 
-const operations = {
-  suma: 1,
-  resta: 2,
-  multiplicacion: 3,
-  division: 4,
-};
+}
 
-formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let formulario = new FormData(e.target);
-
-  let num1 = Number(formulario.get("num1"));
-  let num2 = Number(formulario.get("num2"));
-
-  let operator = Number(formulario.get("selector"));
-
-  let resultado = document.getElementById("resultado");
-
-  switch (operator) {
-    case operations.suma:
-      resultado.innerHTML = Number(num1) + Number(num2);
-      break;
-    case operations.resta:
-      resultado.innerHTML = Number(num1) - Number(num2);
-      break;
-    case operations.multiplicacion:
-      resultado.innerHTML = Number(num1) * Number(num2);
-      break;
-    case operations.division:
-        if ( Number(num2)===0){
-            resultado.innerHTML ="No es posible divir por 0";
-        }
-        else{
-
-            resultado.innerHTML = (Number(num1) / Number(num2)).toFixed(2);
-        }
-      break;
-    default:
-      resultado.innerHTML = "Error";
-  }
-});
+}) 
